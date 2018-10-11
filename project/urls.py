3e0f3d169b087df845223180c971e73990adb6df
Gift-Lumumba <giftlumumba2@gmail.com>
2018-10-12 00:11:31 +0300

    adds ratings route

--- project/urls.py
+++ project/urls.py
@@ -23,5 +23,6 @@ urlpatterns = [
     url(r'^accounts/', include('registration.backends.simple.urls')),
     url(r'^logout/$', views.logout, {"next_page": '/'}), 
     url(r'^tinymce/', include('tinymce.urls')),
+    url(r'^ratings/', include('star_ratings.urls', namespace='ratings', app_name='ratings')),
 
 ]
