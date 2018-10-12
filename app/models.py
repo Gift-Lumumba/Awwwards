dd80fa13e2f7075da9523d15c5c12e43b410623c
Gift-Lumumba <giftlumumba2@gmail.com>
2018-10-12 12:01:34 +0300

    creates new class method search_project  which takes in a title  as a second argument

--- app/models.py
+++ app/models.py
@@ -6,36 +6,13 @@ from django.db.models.signals import post_save
 from django.dispatch import receiver
 
 # Create your models here.
-class Project(models.Model):
-  '''
-  class that contains Project properties
-  '''
-  title = models.CharField(max_length=40,null=True, blank=True)
-  image = models.ImageField(upload_to='images/', null=True,blank=True)
-  description = HTMLField()
-  link = models.URLField(max_length=200)
-
-  def save_project(self):
-    self.save()
-
-  def update_project(self):
-    self.update()
-
-  def delete_project(self):
-    self.delete()
-
-
-  def __str__(self):
-    return self.title
-
 class Profile(models.Model):
   '''
   class that contains user Profile properties
   '''
   profile_pic = models.ImageField(upload_to='images/',null=True, blank=True)
   bio = HTMLField()
-  user = models.OneToOneField(User, on_delete=models.CASCADE,null="True")
-  projects = models.ForeignKey(Project, null=True)
+  user = models.OneToOneField(User, on_delete=models.CASCADE,null=True)
 
   def __str__(self):
     return self.bio
@@ -60,3 +37,39 @@ class Profile(models.Model):
 
   def delete_profile(self):
     self.delete()
+
+  @classmethod
+  def get_profile_by_id(cls,id):
+    profile = Profile.objects.get(id=id)
+    return profile
+
+
+
+class Project(models.Model):
+  '''
+  class that contains Project properties
+  '''
+  title = models.CharField(max_length=40,null=True, blank=True)
+  image = models.ImageField(upload_to='images/', null=True,blank=True)
+  description = HTMLField()
+  link = models.URLField(max_length=200)
+  user = models.ForeignKey(User,on_delete=models.CASCADE,default="")
+  profile = models.ForeignKey(Profile,on_delete=models.CASCADE,default="")
+
+  def save_project(self):
+    self.save()
+
+  def update_project(self):
+    self.update()
+
+  def delete_project(self):
+    self.delete()
+
+  @classmethod
+  def search_project(cls,title):
+    project =  cls.objects.filter(title_icontains=title)
+    return project
+
+  def __str__(self):
+    return self.title
+
