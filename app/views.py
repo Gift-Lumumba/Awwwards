c6e8829fcd0dcbb7c6bd90285b7a2d3b0a5569a6
Gift-Lumumba <giftlumumba2@gmail.com>
2018-10-12 12:19:47 +0300

    creates search_project view function to search for project by title

--- app/views.py
+++ app/views.py
@@ -6,3 +6,16 @@ from django.contrib.auth.decorators import login_required
 @login_required(login_url='/accounts/login/')
 def index(request):
   return render(request,'index.html')
+
+
+def search_results(request):
+  if 'project' in request.GET and request.GET["project"]:
+    title = request.GET.get("project")
+    searched_projects = Project.search_project(title)
+    message = f"{title}"
+
+    return render(request, 'search.html',{"message":message,"projects":searched_projects})
+
+  else:
+    message = "You haven't searched for anything"
+    return render(request, 'search.html',{"message":message})
