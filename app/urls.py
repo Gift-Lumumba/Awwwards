8f704a4b67345dcd655fb299f862435b1c6406c7
Gift-Lumumba <giftlumumba2@gmail.com>
2018-10-14 02:45:14 +0300

    creates new URLConf to handle requests to models

--- app/urls.py
+++ app/urls.py
@@ -7,6 +7,14 @@ urlpatterns=[
     url('^$',views.index,name = 'index'),
     url(r'^search/', views.search_results, name='search_results'),
     url(r'^project/$', views.upload_project, name='upload_project'),
+    url(r'^user/(\d+)$', views.profile, name='profile'),
+    url(r'^update/',views.update_profile,name='update_profile'),
+    url(r'^api/projects/$', views.ProjectList.as_view()),
+    url(r'api/projects/project-id/(?P<pk>[0-9]+)/$',
+    views.ProjectDescription.as_view()),
+    url(r'^api/profiles/$', views.ProfileList.as_view()),
+    url(r'api/profiles/profile-id/(?P<pk>[0-9]+)/$',
+    views.ProfileDescription.as_view())
 ]
 
 if settings.DEBUG:
