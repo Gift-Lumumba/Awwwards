b049c3ed4a39f0521c3d08487c78c30082e94c15
Gift-Lumumba <giftlumumba2@gmail.com>
2018-10-14 02:44:08 +0300

    creates ProjectList,ProjectDescription,ProfileList and ProfileDescription classes that inherit from the APIView class

--- app/views.py
+++ app/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render,redirect
+from django.contrib.auth.models import User
 from django.contrib.auth.decorators import login_required
-from .forms import ProjectForm
-from . models import Profile,Project
+from django.http  import HttpResponse,Http404,HttpResponseRedirect
+from .forms import ProjectForm,UpdateProfileForm
+from .models import Profile,Project
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from .serializer import ProjectSerializer,ProfileSerializer
+from rest_framework import status
+from .permissions import IsAdminOrReadOnly
 
 
 # Create your views here.
@@ -47,10 +54,108 @@ def upload_project(request):
 
 @login_required(login_url='/accounts/login/')
 def profile(request, user_id):
-    '''
-    View function that enables users see their profile
-    '''
+    """
+    Function that enables one to see their profile
+    """
     title = "Profile"
-    profiles = Profile.objects.all()
+    projects = Project.get_project_by_id(id= user_id).order_by('-posted_on')
+    # profiles = User.objects.get(id=user_id)
+    users = User.objects.get(id=user_id)
 
-    return render(request,'profile.html',locals())
+    return render(request, 'profile/profile.html',locals())
+
+    
+@login_required(login_url='/accounts/login/')
+def update_profile(request):
+   if request.method == 'POST':
+        form = UpdateProfileForm(request.POST, request.FILES)
+        if form.is_valid():
+            profile = form.save(commit=False)
+            profile.save()
+        return redirect('index')
+   else:
+        form = UpdateProfileForm()
+   return render(request, 'profile/update_profile.html', locals())
+
+class ProjectList(APIView):
+    def get(self, request, format=None):
+        all_projects = Project.objects.all()
+        serializers = ProjectSerializer(all_projects, many=True)
+        return Response(serializers.data)
+
+    permission_classes = (IsAdminOrReadOnly,)
+    def post(self, request, format=None):
+        serializers = ProjectSerializer(data=request.data)
+        if serializers.is_valid():
+            serializers.save()
+            return Response(serializers.data, status=status.HTTP_201_CREATED)
+        return Response(serializers.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class ProjectDescription(APIView):
+    permission_classes = (IsAdminOrReadOnly,)
+    def get_project(self, pk):
+        try:
+            return Project.objects.get(pk=pk)
+        except Project.DoesNotExist:
+            return Http404
+
+    def get(self, request, pk, format=None):
+        project = self.get_project(pk)
+        serializers = ProjectSerializer(project)
+        return Response(serializers.data)
+
+    def put(self, request, pk, format=None):
+        project = self.get_project(pk)
+        serializers = ProjectSerializer(project, request.data)
+        if serializers.is_valid():
+            serializers.save()
+            return Response(serializers.data)
+        else:
+            return Response(serializers.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk, format=None):
+        project = self.get_project(pk)
+        project.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+class ProfileList(APIView):
+    def get(self, request, format=None):
+        all_profiles = Profile.objects.all()
+        serializers = ProfileSerializer(all_profiles, many=True)
+        return Response(serializers.data)
+
+    permission_classes = (IsAdminOrReadOnly,)
+    def post(self, request, format=None):
+        serializers = ProfileSerializer(data=request.data)
+        if serializers.is_valid():
+            serializers.save()
+            return Response(serializers.data, status=status.HTTP_201_CREATED)
+        return Response(serializers.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class ProfileDescription(APIView):
+    permission_classes = (IsAdminOrReadOnly,)
+    def get_profile(self, pk):
+        try:
+            return Profile.objects.get(pk=pk)
+        except Profile.DoesNotExist:
+            return Http404
+
+    def get(self, request, pk, format=None):
+        profile = self.get_profile(pk)
+        serializers = ProfileSerializer(profile)
+        return Response(serializers.data)
+
+    def put(self, request, pk, format=None):
+        profile = self.get_profile(pk)
+        serializers = ProfileSerializer(profile, request.data)
+        if serializers.is_valid():
+            serializers.save()
+            return Response(serializers.data)
+        else:
+            return Response(serializers.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk, format=None):
+        profile = self.get_profile(pk)
+        profile.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
