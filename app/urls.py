57252ad8b055c47060857b885aaa5ecee0ea13d9
Gift-Lumumba <giftlumumba2@gmail.com>
2018-10-14 20:48:41 +0300

    adds urls to activate and signup

--- app/urls.py
+++ app/urls.py
@@ -5,6 +5,9 @@ from django.conf.urls.static import static
 
 urlpatterns=[
     url('^$',views.index,name = 'index'),
+    url(r'^signup/$', views.signup, name='signup'),
+    url(r'^activate/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
+        views.activate, name='activate'),
     url(r'^search/', views.search_results, name='search_results'),
     url(r'^project/$', views.upload_project, name='upload_project'),
     url(r'^user/(\d+)$', views.profile, name='profile'),
