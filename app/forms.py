733dd472f02ceb2560ae96beeb8bf9ef9927288c
Gift-Lumumba <giftlumumba2@gmail.com>
2018-10-15 16:25:11 +0300

    adds ContentForm,UsabilityForm and DesignForm classes

--- app/forms.py
+++ app/forms.py
@@ -1,7 +1,7 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
-from .models import Profile,Project
+from .models import Profile,Project,UsabilityRating,DesignRating,ContentRating
 
 class SignupForm(UserCreationForm):
     email = forms.EmailField(max_length=200, help_text='Required')
@@ -14,11 +14,28 @@ class SignupForm(UserCreationForm):
 class ProjectForm(forms.ModelForm):
     class Meta:
         model = Project
-        exclude =['posted_on','profile','user']
+        exclude =['posted_on','profile','user','rating',]
 
 
 class UpdateProfileForm(forms.ModelForm):
     class Meta:
         model = Profile
         exclude = ['user']
-        fields = ('profile_pic','bio' )
+
+
+class ContentForm(forms.ModelForm):
+    class Meta:
+        model = ContentRating
+        fields = ['rating',]
+
+
+class UsabilityForm(forms.ModelForm):
+    class Meta:
+        model = UsabilityRating
+        fields = ['rating', ]
+
+
+class DesignForm(forms.ModelForm):
+    class Meta:
+        model = DesignRating
+        fields = ['rating', ]
