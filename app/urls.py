38b9338839c8256abf138325184f2d26d2f7bcef
Gift-Lumumba <giftlumumba2@gmail.com>
2018-10-15 16:34:38 +0300

    adds routes to post api requests

--- app/urls.py
+++ app/urls.py
@@ -5,19 +5,23 @@ from django.conf.urls.static import static
 
 urlpatterns=[
     url('^$',views.index,name = 'index'),
-    url(r'^signup/$', views.signup, name='signup'),
-    url(r'^activate/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
-        views.activate, name='activate'),
+    # url(r'^signup/$', views.signup, name='signup'),
+    # url(r'^activate/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
+    #     views.activate, name='activate'),
     url(r'^search/', views.search_results, name='search_results'),
     url(r'^project/$', views.upload_project, name='upload_project'),
     url(r'^user/(\d+)$', views.profile, name='profile'),
-    url(r'^update/',views.update_profile,name='update_profile'),
+    url(r'^profile/update/$', views.update_profile, name='update_profile'),
     url(r'^api/projects/$', views.ProjectList.as_view()),
     url(r'api/projects/project-id/(?P<pk>[0-9]+)/$',
     views.ProjectDescription.as_view()),
     url(r'^api/profiles/$', views.ProfileList.as_view()),
     url(r'api/profiles/profile-id/(?P<pk>[0-9]+)/$',
-    views.ProfileDescription.as_view())
+    views.ProfileDescription.as_view()),
+    url(r'^post/(?P<project_id>[0-9]+)/review_design/$', views.add_design, name='add_design'),
+    url(r'^post/(?P<project_id>[0-9]+)/review_usability/$', views.add_usability, name='review_usability'),
+    url(r'^post/(?P<project_id>[0-9]+)/review_content/$', views.add_content, name='review_content'),
+
 ]
 
 if settings.DEBUG:
